7f27aada742aee90284036eb5766f14acc44e44a
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-05 19:13:30 +0100

    update

--- CitiesManager.Web/DatabaseContext/ApplicationDbContext.cs
+++ CitiesManager.Web/DatabaseContext/ApplicationDbContext.cs
@@ -21,5 +21,5 @@ public class ApplicationDbContext : DbContext
         modelBuilder.Entity<City>().HasData(new City() { CityId= Guid.Parse("CDBE0726 - BAFB - 4D1F - 8B14 - 12364CF2E139"), CityName="New York" });
         modelBuilder.Entity<City>().HasData(new City() { CityId = Guid.Parse("FF968DC0-1BC0-4A77-9BDC-8BDA6A91106B"), CityName="Shenzo" });
     }
-    //tools>create guid>select 4 registry format> copy generated guid
+    //x generare guid: tools>create guid>select 4 registry format> copy generated guid
 }
